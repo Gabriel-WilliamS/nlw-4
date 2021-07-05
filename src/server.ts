27f771a1974034f2876a0e5b0fdc1d887dc36500
Gabriel William <gabrielwilliamcps@gmail.com>
2021-07-05 00:04:45 -0300

    02v Aula nlw#4

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
+import "reflect-metadata";
 import express from "express";
+import "./database";
+import { router } from "./routes";
 
 const app = express();
 
-app.get("/users");
+app.use(express.json());
+app.use(router);
 
-app.listen(3333, () => console.log("Sever is running port 3333"));
+app.listen(3000, () => console.log("Sever is running port 3000"));
